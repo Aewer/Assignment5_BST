af7bcdcc071d968dedd6e368952a34bcd8ed6653
aewer <slawa.miroshnik.2004@gmail.com>
2023-05-22 00:31:19 +0600

    Added constructor to Node

--- src/BST.java
+++ src/BST.java
@@ -4,5 +4,9 @@ public class BST<K extends Comparable<K>, V> {
         private K key;
         private V val;
         private Node left, right;
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
     }
 }
