db14167a375e0837a15f34333e6d583958ed1deb
aewer <slawa.miroshnik.2004@gmail.com>
2023-05-22 00:50:05 +0600

    Added put method

--- src/BST.java
+++ src/BST.java
@@ -13,4 +13,18 @@ public class BST<K extends Comparable<K>, V> {
     public int size() {
         return size;
     }
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+    public Node put(Node current, K key, V value) {
+        if (current == null) {
+            return new Node(key, value);
+        }
+        if(key.compareTo(current.key) < 0) {
+            current.left = put(current.left, key, value);
+        } else if (key.compareTo(current.key) > 0) {
+            current.right = put(current.right, key, value);
+        }
+        return current;
+    }
 }
